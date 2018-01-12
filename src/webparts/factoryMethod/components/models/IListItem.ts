db459132377700dc7bd32a2a82cf77f428c35cd5
Luis Valencia <luisevalencia@gmail.com>
2018-01-12 16:31:21 +0100

    added src back

---
+++ src/webparts/factoryMethod/components/models/IListItem.ts
@@ -0,0 +1,12 @@
+export  interface IListItem {
+    [key: string]: any;
+    id: string;
+    title: string;
+    modified: Date;
+    created: Date;
+    modifiedby: string;
+    createdby: string;
+}
+
+
+
---
+++ src/webparts/factoryMethod/components/models/INewsListItem.ts
@@ -0,0 +1,8 @@
+
+import {IListItem} from "./IListItem";
+
+export interface  INewsListItem extends IListItem {
+    newsheader: string;
+    newsbody: string;
+    expiryDate: Date;
+}
